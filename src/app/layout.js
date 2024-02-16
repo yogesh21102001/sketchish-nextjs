4088192a09a3696d5cc50246b494d60d4ee6caaa
Yogesh <yogeshv205.ygcygc@gmail.com>
2024-02-16 17:12:16 +0530

    Initial

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,13 +7,6 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   metadataBase: new URL('https://sketchish.com'),
-  alternates: {
-    canonical: '/',
-    languages: {
-      'en-US': '/en-US',
-      'de-DE': '/de-DE',
-    },
-  },
   openGraph: {
     images: './opengraph-image.png',
   },
@@ -23,44 +16,15 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
-
-  const CrispWithNoSSR = dynamic(
-    () => import('../components/CrispChat/CrispChat.jsx')
-  )
-  const HotJarNoSSR = dynamic(
-    () => import('../components/HotJar/HotJar.jsx')
-  )
-  const GoogleTagManagerNoSSR = dynamic(
-    () => import('../components/GoogleTagManager/GoogleTagManager.jsx')
-  )
-  
 
   return (
     <html lang="en">
       <header>
-
         <meta property="og:type" content="website"></meta>
         <meta property="og:image" content="https://sketchish.com/opengraph-image.png?67f65930403b8043"></meta>
         <meta name="twitter:image" content="https://sketchish.com/opengraph-image.png?67f65930403b8043" />
-        <script
-          async
-          src='https://www.googletagmanager.com/gtag/js?id=GTM-5JDQQH6D'
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'GTM-5JDQQH6D');`
-          }}
-        ></script>
       </header>
       <body className={inter.className}>
-        <GoogleTagManagerNoSSR />
-        <CrispWithNoSSR />
-        <HotJarNoSSR />
         <NavBar />
         {children}
       </body>
